3895d2377fb5e308c4a1e057c6a6f49f89adccd5
Nate Wright <natew@notthisway.com>
2016-11-25 17:56:34 +0000

    Don't display opening hours component if no setting exists

--- components/templates/opening-hours.php
+++ components/templates/opening-hours.php
@@ -4,9 +4,11 @@
  *
  * @since 0.1
  */
+if ( totcbase_bp_setting_exists( 'opening-hours' ) ) :
 ?>
 
 <div class="clc-wrapper clc-opening-hours">
 	[contact-card show_name=0 show_address=0 show_get_directions=0 show_phone=0 show_contact=0 show_booking_link=0 show_map=0]
 </div>
 <?php
+endif;
