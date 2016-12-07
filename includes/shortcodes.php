8ecfe7dcb337deb992dbfd49d4b749d9ffb4663d
Nate Wright <natew@notthisway.com>
2016-12-07 20:38:12 +0000

    Move Pages component over to a shortcode

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -10,8 +10,8 @@
  * that class exists. If not, it falls back to WP_Widget_Recent_Posts.
  *
  * @param array $args {
- *  `number` The number of posts to display. Default: 3
- *  `show_date` Whether or not to show the date. Default: true
+ *  `number` int The number of posts to display. Default: 3
+ *  `show_date` bool Whether or not to show the date. Default: true
  * }
  * @since 0.1
  */
@@ -37,3 +37,62 @@ function totclc_shortcode_recent_posts( $args = array() ) {
 
 }
 add_shortcode( 'totclc-recent-posts', 'totclc_shortcode_recent_posts' );
+
+/**
+* Print a title, excerpt, thumbnail and read more link for a single post or a
+* collection of posts.
+*
+* Expects to be Pages or Page-like content, so no date or author information is
+* printed.
+*
+* @param array $args {
+*  `post_ids` string List of post IDs, separated by a comma. Required
+*  `post_type` string List of post type(s) to display. Default: 'page'
+* }
+* @since 0.1
+*/
+function totclc_shortcode_pages( $args = array() ) {
+
+	$defaults = array(
+		'post_ids'  => '',
+		'post_type' => 'page',
+	);
+
+	$atts = shortcode_atts( $defaults, $args, 'totclc-pages' );
+
+	$post_type = explode( ',', $atts['post_type'] );
+
+	if ( !isset( $atts['post_ids'] ) ) {
+		return '';
+	} else {
+		$post_ids = explode( ',', $atts['post_ids'] );
+	}
+
+	$post_query = new WP_Query( array( 'post__in' => $post_ids, 'post_type' => $post_type ) );
+
+	ob_start();
+	?>
+
+	<?php while( $post_query->have_posts() ) : $post_query->the_post(); ?>
+		<article id="post-<?php echo (int) get_the_ID(); ?>" <?php post_class(); ?>>
+			<?php the_post_thumbnail(); ?>
+			<header class="entry-header">
+				<?php the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+			</header><!-- .entry-header -->
+			<div class="entry-content">
+				<?php the_excerpt(); ?>
+				<a href="<?php the_permalink(); ?>" class="more">
+					<?php
+					    // Translators: 1 and 3 are an opening and closing <span> tag. 2 is the post title.
+					    printf( esc_html__( 'Read More%1$s about %2$s%3$s', 'totc-layout-control' ), '<span class="screen-reader-text">', esc_html( get_the_title() ), '</span>' );
+					?>
+				</a>
+			</div><!-- .entry-content -->
+		</article><!-- #post-## -->
+	<?php endwhile; ?>
+	<?php
+	wp_reset_query();
+
+	return ob_get_clean();
+}
+add_shortcode( 'totclc-pages', 'totclc_shortcode_pages' );
