bc246ce841eb18372778d601ba2591b3ef1067af
Nate Wright <natew@notthisway.com>
2016-12-07 21:06:49 +0000

    Fix class name in map template

--- components/templates/map.php
+++ components/templates/map.php
@@ -8,7 +8,7 @@ global $bpfwp_controller;
 if ( isset( $bpfwp_controller ) && $bpfwp_controller->settings->get_setting( 'address' ) ) :
 ?>
 
-<div class="clc-wrapper clc-opening-hours">
+<div class="clc-wrapper clc-map">
 	[contact-card show_name=0 show_address=0 show_get_directions=0 show_phone=0 show_contact=0 show_booking_link=0 show_opening_hours=0]
 </div>
 <?php
