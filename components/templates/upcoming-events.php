c4021c26559873afc9034c27a66cadb0560e7ba1
Nate Wright <natew@notthisway.com>
2016-12-07 21:07:43 +0000

    Fix class name in upcoming events template

--- components/templates/upcoming-events.php
+++ components/templates/upcoming-events.php
@@ -5,7 +5,7 @@
  * @since 0.1
  */
 ?>
-<div class="clc-wrapper clc-recent-posts clc-recent-posts-<?php echo absint( $this->number ); ?>">
+<div class="clc-wrapper clc-upcoming-events clc-upcoming-events-<?php echo absint( $this->number ); ?>">
 	<h2 class="title"><?php echo esc_html( $this->title ); ?></h2>
 	[eo_events numberposts="<?php echo absint( $this->number ); ?>" showpastevents="false"]
 </div>
