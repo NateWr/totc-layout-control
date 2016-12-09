cadb57fdb83a135fe4a5f60324b2a3bd1491e7d4
Nate Wright <natew@notthisway.com>
2016-12-09 16:28:58 +0000

    Add class with number of locations to location template

--- components/templates/locations.php
+++ components/templates/locations.php
@@ -9,7 +9,7 @@ if ( isset( $bpfwp_controller ) && $bpfwp_controller->settings->get_setting( 'mu
 	$post_query = new WP_Query( array( 'posts_per_page' => 100, 'post_type' => array( 'location' ) ) );
 ?>
 
-<div class="clc-wrapper clc-locations">
+<div class="clc-wrapper clc-locations-<?php echo absint( $post_query->found_posts ); ?>">
 	<?php while( $post_query->have_posts() ) : $post_query->the_post(); ?>
 		<article id="post-<?php echo (int) get_the_ID(); ?>" <?php post_class(); ?>><?php
 			?>[contact-card location="<?php echo (int) get_the_ID(); ?>" show_name=0 show_address=0 show_get_directions=0 show_phone=0 show_contact=0 show_booking_link=0 show_opening_hours=0]<?php
