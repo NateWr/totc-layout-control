16edd5d5e6e4cf6fffa9e8b26c7e57caaa724c1b
Nate Wright <natew@notthisway.com>
2017-02-22 17:25:42 +0000

    Use medium images in gallery by default

--- components/templates/gallery.php
+++ components/templates/gallery.php
@@ -2,11 +2,12 @@
 /**
  * Layout template for the Gallery component
  *
+ * @param $this->images array List of attachment IDs
  * @since 0.1
  */
 ?>
 
 <div class="clc-wrapper clc-gallery">
-	[gallery link="file" ids="<?php echo esc_attr( join( ',', $this->images ) ); ?>"]
+	[gallery link="file" ids="<?php echo esc_attr( join( ',', $this->images ) ); ?>" size="medium"]
 </div>
 <?php
